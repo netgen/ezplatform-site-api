afc7de07f3e77739ecc02b830c08c737198d2e42
Petar Španja <petar@spanja.info>
2016-08-01 19:59:26 +0200

    Docblock: Add missing propery hints

--- lib/API/Values/Field.php
+++ lib/API/Values/Field.php
@@ -13,6 +13,8 @@ use eZ\Publish\API\Repository\Values\ValueObject;
  * @property-read string|int $id
  * @property-read string $identifier
  * @property-read string $fieldTypeIdentifier
+ * @property-read string $name
+ * @property-read string $description
  * @property-read \eZ\Publish\SPI\FieldType\Value $value
  * @property-read \Netgen\EzPlatformSite\API\Values\Content $content
  * @property-read \eZ\Publish\API\Repository\Values\Content\Field $innerField

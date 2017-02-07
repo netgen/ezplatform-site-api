d482931f3c7cd1b0a01844e8348f309dacfa2e9a
Petar Španja <petar@spanja.info>
2017-02-07 14:41:59 +0100

    Removed ContentTrait

--- lib/Core/Site/Values/Content.php
+++ lib/Core/Site/Values/Content.php
@@ -6,7 +6,25 @@ use Netgen\EzPlatformSiteApi\API\Values\Content as APIContent;
 
 final class Content extends APIContent
 {
-    use ContentTrait;
+    /**
+     * @var \Netgen\EzPlatformSiteApi\API\Values\ContentInfo
+     */
+    protected $contentInfo;
+
+    /**
+     * @var \Netgen\EzPlatformSiteApi\API\Values\Field[]
+     */
+    protected $fields = [];
+
+    /**
+     * @var \eZ\Publish\API\Repository\Values\Content\Content
+     */
+    protected $innerContent;
+
+    /**
+     * @var \eZ\Publish\API\Repository\Values\Content\Field[]
+     */
+    private $fieldsById = [];
 
     public function __construct(array $properties = [])
     {
@@ -19,5 +37,110 @@ final class Content extends APIContent
         }
 
         parent::__construct($properties);
+    }
+
+    public function hasField($identifier)
+    {
+        return isset($this->fields[$identifier]);
+    }
+
+    public function getField($identifier)
+    {
+        if ($this->hasField($identifier)) {
+            return $this->fields[$identifier];
+        }
+
+        return null;
+    }
+
+    public function hasFieldById($id)
+    {
+        return isset($this->fieldsById[$id]);
+    }
+
+    public function getFieldById($id)
+    {
+        if ($this->hasFieldById($id)) {
+            return $this->fieldsById[$id];
+        }
+
+        return null;
+    }
+
+    public function getFieldValue($identifier)
+    {
+        if ($this->hasField($identifier)) {
+            return $this->fields[$identifier]->value;
+        }
+
+        return null;
+    }
+
+    public function getFieldValueById($id)
+    {
+        if ($this->hasFieldById($id)) {
+            return $this->fieldsById[$id]->value;
+        }
+
+        return null;
+    }
+
+    /**
+     * Magic getter for retrieving convenience properties.
+     *
+     * @param string $property The name of the property to retrieve
+     *
+     * @return mixed
+     */
+    public function __get($property)
+    {
+        switch ($property) {
+            case 'id':
+                return $this->contentInfo->id;
+            case 'name':
+                return $this->contentInfo->name;
+            case 'mainLocationId':
+                return $this->contentInfo->mainLocationId;
+        }
+
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        } elseif (property_exists($this->innerContent, $property)) {
+            return $this->innerContent->$property;
+        }
+
+        return parent::__get($property);
+    }
+
+    /**
+     * Magic isset for signaling existence of convenience properties.
+     *
+     * @param string $property
+     *
+     * @return bool
+     */
+    public function __isset($property)
+    {
+        switch ($property) {
+            case 'id':
+            case 'name':
+            case 'mainLocationId':
+                return true;
+        }
+
+        if (property_exists($this, $property) || property_exists($this->innerContent, $property)) {
+            return true;
+        }
+
+        return parent::__isset($property);
+    }
+
+    private function buildField(array $properties = [])
+    {
+        $properties['content'] = $this;
+        $field = new Field($properties);
+
+        $this->fields[$field->fieldDefIdentifier] = $field;
+        $this->fieldsById[$field->id] = $field;
     }
 }

7e77d9ef66de1030fcaa1203f9b1edcc11c016d3
Edi Modrić <edi.modric@gmail.com>
2018-02-15 15:20:44 +0100

    Fix the ContentValueView interface :o)

--- bundle/View/ContentValueView.php
+++ bundle/View/ContentValueView.php
@@ -7,9 +7,9 @@ use eZ\Publish\Core\MVC\Symfony\View\ContentValueView as BaseContentValueView;
 interface ContentValueView extends BaseContentValueView
 {
     /**
-     * Returns the Site Location.
+     * Returns the Site Content.
      *
-     * @return \Netgen\EzPlatformSiteApi\API\Values\Location
+     * @return \Netgen\EzPlatformSiteApi\API\Values\Content
      */
-    public function getSiteLocation();
+    public function getSiteContent();
 }

756e336a1611a94eb574981c2f8184b158799304
Petar Španja <petar@spanja.info>
2018-05-30 15:32:30 +0200

    Use new Pagerfanta adapters

--- bundle/QueryType/QueryExecutor.php
+++ bundle/QueryType/QueryExecutor.php
@@ -7,10 +7,8 @@ use eZ\Publish\API\Repository\Values\Content\Query;
 use eZ\Publish\Core\QueryType\QueryTypeRegistry;
 use Netgen\EzPlatformSiteApi\API\FilterService;
 use Netgen\EzPlatformSiteApi\API\FindService;
-use Netgen\EzPlatformSiteApi\Core\Site\Pagination\Pagerfanta\ContentSearchAdapter;
-use Netgen\EzPlatformSiteApi\Core\Site\Pagination\Pagerfanta\ContentSearchFilterAdapter;
-use Netgen\EzPlatformSiteApi\Core\Site\Pagination\Pagerfanta\LocationSearchAdapter;
-use Netgen\EzPlatformSiteApi\Core\Site\Pagination\Pagerfanta\LocationSearchFilterAdapter;
+use Netgen\EzPlatformSiteApi\Core\Site\Pagination\Pagerfanta\FilterAdapter;
+use Netgen\EzPlatformSiteApi\Core\Site\Pagination\Pagerfanta\FindAdapter;
 use Pagerfanta\Pagerfanta;
 use RuntimeException;
 
@@ -67,46 +65,58 @@ final class QueryExecutor
         $queryType = $this->queryTypeRegistry->getQueryType($queryDefinition->name);
         $query = $queryType->getQuery($queryDefinition->parameters);
 
+        if ($usePager) {
+            return $this->getPager($query, $queryDefinition);
+        }
+
         if ($query instanceof LocationQuery) {
-            return $this->getLocationResult($query, $queryDefinition, $usePager);
+            return $this->getLocationResult($query, $queryDefinition);
         }
 
         if ($query instanceof Query) {
-            return $this->getContentResult($query, $queryDefinition, $usePager);
+            return $this->getContentResult($query, $queryDefinition);
         }
 
         throw new RuntimeException('Could not handle given query');
     }
 
     /**
-     * Return search result by the given parameters.
+     * Return Pagerfanta instance by the given parameters.
      *
      * @throws \Pagerfanta\Exception\Exception
      *
-     * @param \eZ\Publish\API\Repository\Values\Content\LocationQuery $query
+     * @param \eZ\Publish\API\Repository\Values\Content\Query $query
      * @param \Netgen\Bundle\EzPlatformSiteApiBundle\QueryType\QueryDefinition $queryDefinition
-     * @param bool $usePager
      *
-     * @return \eZ\Publish\API\Repository\Values\Content\Search\SearchResult|\Pagerfanta\Pagerfanta
+     * @return \Pagerfanta\Pagerfanta
      */
-    private function getLocationResult(LocationQuery $query, QueryDefinition $queryDefinition, $usePager)
+    private function getPager(Query $query, QueryDefinition $queryDefinition)
     {
-        if ($usePager) {
-            if ($queryDefinition->useFilter) {
-                $adapter = new LocationSearchFilterAdapter($query, $this->filterService);
-            } else {
-                $adapter = new LocationSearchAdapter($query, $this->findService);
-            }
-
-            $pager = new Pagerfanta($adapter);
-
-            $pager->setNormalizeOutOfRangePages(true);
-            $pager->setMaxPerPage($queryDefinition->maxPerPage);
-            $pager->setCurrentPage($queryDefinition->page);
-
-            return $pager;
+        if ($queryDefinition->useFilter) {
+            $adapter = new FilterAdapter($query, $this->filterService);
+        } else {
+            $adapter = new FindAdapter($query, $this->findService);
         }
 
+        $pager = new Pagerfanta($adapter);
+
+        $pager->setNormalizeOutOfRangePages(true);
+        $pager->setMaxPerPage($queryDefinition->maxPerPage);
+        $pager->setCurrentPage($queryDefinition->page);
+
+        return $pager;
+    }
+
+    /**
+     * Return search result by the given parameters.
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\LocationQuery $query
+     * @param \Netgen\Bundle\EzPlatformSiteApiBundle\QueryType\QueryDefinition $queryDefinition
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Search\SearchResult
+     */
+    private function getLocationResult(LocationQuery $query, QueryDefinition $queryDefinition)
+    {
         if ($queryDefinition->useFilter) {
             return $this->filterService->filterLocations($query);
         }
@@ -117,32 +127,13 @@ final class QueryExecutor
     /**
      * Return search result by the given parameters.
      *
-     * @throws \Pagerfanta\Exception\Exception
-     *
      * @param \eZ\Publish\API\Repository\Values\Content\Query $query
      * @param \Netgen\Bundle\EzPlatformSiteApiBundle\QueryType\QueryDefinition $queryDefinition
-     * @param bool $usePager
      *
-     * @return \eZ\Publish\API\Repository\Values\Content\Search\SearchResult|\Pagerfanta\Pagerfanta
+     * @return \eZ\Publish\API\Repository\Values\Content\Search\SearchResult
      */
-    private function getContentResult(Query $query, QueryDefinition $queryDefinition, $usePager)
+    private function getContentResult(Query $query, QueryDefinition $queryDefinition)
     {
-        if ($usePager) {
-            if ($queryDefinition->useFilter) {
-                $adapter = new ContentSearchFilterAdapter($query, $this->filterService);
-            } else {
-                $adapter = new ContentSearchAdapter($query, $this->findService);
-            }
-
-            $pager = new Pagerfanta($adapter);
-
-            $pager->setNormalizeOutOfRangePages(true);
-            $pager->setMaxPerPage($queryDefinition->maxPerPage);
-            $pager->setCurrentPage($queryDefinition->page);
-
-            return $pager;
-        }
-
         if ($queryDefinition->useFilter) {
             return $this->filterService->filterContent($query);
         }

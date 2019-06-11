a1c7cd38a488e8c2545a2e73efc96b6331231c1a
Petar Španja <petar@spanja.info>
2019-06-11 20:24:00 +0200

    Complete Field tests

--- tests/lib/Unit/Core/Site/Values/FieldsTest.php
+++ tests/lib/Unit/Core/Site/Values/FieldsTest.php
@@ -14,15 +14,19 @@ use eZ\Publish\Core\Repository\Values\ContentType\ContentType;
 use eZ\Publish\Core\Repository\Values\ContentType\FieldDefinition;
 use eZ\Publish\SPI\FieldType\FieldType;
 use Netgen\EzPlatformSiteApi\API\Site;
+use Netgen\EzPlatformSiteApi\API\Values\Field as SiteField;
+use Netgen\EzPlatformSiteApi\API\Values\Fields as APIFields;
 use Netgen\EzPlatformSiteApi\Core\Site\DomainObjectMapper;
 use Netgen\EzPlatformSiteApi\Core\Site\Values\Content;
 use Netgen\EzPlatformSiteApi\Core\Site\Values\Fields;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use RuntimeException;
 
 /**
- * @group fields
  * Fields value unit tests.
  *
+ * @group fields
  * @see \Netgen\EzPlatformSiteApi\API\Values\Fields
  */
 class FieldsTest extends TestCase
@@ -72,20 +76,257 @@ class FieldsTest extends TestCase
      */
     private $fieldTypeMock;
 
-    public function testFieldsObjectIsCountable()
-    {
-        $fields = $this->getFieldsObjectUnderTest();
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $loggerMock;
 
-        $this->assertCount(1, $fields);
+    public function testFieldsCanBeCounted()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        $this->assertEquals(3, count($fields));
     }
 
-    protected function getFieldsObjectUnderTest()
+    /**
+     * @depends testFieldsCanBeCounted
+     */
+    public function testFieldsCanBeIterated()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+        $i = 1;
+
+        foreach ($fields as $field) {
+            $this->assertInstanceOf(APIFields::class, $fields);
+            $this->assertEquals($i, $field->id);
+            $i++;
+        }
+    }
+
+    /**
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testExistenceOfExistingFieldCanBeCheckedByIdentifier()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        $this->assertTrue($fields->hasField('first'));
+    }
+
+    /**
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testExistenceOfNonExistingFieldCanBeCheckedByIdentifier()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        $this->assertFalse($fields->hasField('fourth'));
+    }
+
+    public function testExistenceOfExistingFieldCanBeCheckedAsAnArrayByIdentifier()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        $this->assertTrue(isset($fields['first']));
+    }
+
+    public function testExistenceOfNonExistingFieldCanBeCheckedAsAnArrayByIdentifier()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        $this->assertFalse(isset($fields['fourth']));
+    }
+
+    public function testExistenceOfExistingFieldCanBeCheckedAsAnArrayByNumericIndex()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        $this->assertTrue(isset($fields[0]));
+    }
+
+    public function testExistenceOfNonExistingFieldCanBeCheckedAsAnArrayByNumericIndex()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        $this->assertFalse(isset($fields[101]));
+    }
+
+    public function testFieldsCanBeAccessedAsAnArrayByNumericIndex()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        for ($i = 0; $i < 3; $i++) {
+            $field = $fields[$i];
+            $this->assertInstanceOf(SiteField::class, $field);
+            $this->assertEquals($i + 1, $field->id);
+        }
+    }
+
+    public function testFieldsCanBeAccessedAsAnArrayByIdentifier()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+        $identifiers = ['first', 'second', 'third'];
+
+        foreach ($identifiers as $identifier) {
+            $field = $fields[$identifier];
+            $this->assertInstanceOf(SiteField::class, $field);
+            $this->assertEquals($identifier, $field->fieldDefIdentifier);
+        }
+    }
+
+    public function testAccessingNonExistentFieldThrowsRuntimeException()
+    {
+        $this->expectException(RuntimeException::class);
+
+        $fields = $this->getFieldsUnderTest(true);
+
+        $fields['fourth'];
+    }
+
+    public function testAccessingNonExistentFieldReturnsNullField()
+    {
+        $fields = $this->getFieldsUnderTest(false);
+        $identifier = 'fourth';
+
+        $loggerMock = $this->getLoggerMock();
+        $loggerMock
+            ->expects($this->once())
+            ->method('critical')
+            ->with('Field "fourth" in Content #1 does not exist, using null field instead');
+
+        $field = $fields[$identifier];
+
+        $this->assertInstanceOf(SiteField::class, $field);
+        $this->assertEquals($identifier, $field->fieldDefIdentifier);
+        $this->assertEquals('ngnull', $field->fieldTypeIdentifier);
+        $this->assertTrue($field->isEmpty());
+    }
+
+    public function testFieldCanNotBeSet()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Setting the field to the collection is not allowed');
+
+        $fields = $this->getFieldsUnderTest(true);
+
+        /** @noinspection OnlyWritesOnParameterInspection */
+        $fields['pekmez'] = 'džem';
+    }
+
+    public function testFieldCanNotBeUnset()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Unsetting the field from the collection is not allowed');
+
+        $fields = $this->getFieldsUnderTest(true);
+
+        unset($fields['first']);
+    }
+
+    /**
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testExistenceOfExistingFieldCanBeCheckedById()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        $this->assertTrue($fields->hasFieldById(1));
+    }
+
+    /**
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testExistenceOfNonExistingFieldCanBeCheckedById()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        $this->assertFalse($fields->hasFieldById(101));
+    }
+
+    /**
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testExistingFieldCanBeAccessedById()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+        $id = 1;
+
+        $field = $fields->getFieldById($id);
+
+        $this->assertInstanceOf(SiteField::class, $field);
+        $this->assertEquals($id, $field->id);
+    }
+
+    /**
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testNonExistentFieldCanNotBeAccessedById()
+    {
+        $id = 101;
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage("Field #{$id} in Content #1 does not exist");
+
+        $fields = $this->getFieldsUnderTest(true);
+
+        $fields->getFieldById($id);
+    }
+
+    /**
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testAccessingNonExistentFieldByIdReturnsNullField()
+    {
+        $id = 101;
+
+        $fields = $this->getFieldsUnderTest(false);
+
+        $field = $fields->getFieldById($id);
+
+        $this->assertInstanceOf(SiteField::class, $field);
+        $this->assertEquals((string)$id, $field->fieldDefIdentifier);
+        $this->assertEquals('ngnull', $field->fieldTypeIdentifier);
+        $this->assertTrue($field->isEmpty());
+    }
+
+    public function testDebugDump()
+    {
+        $fields = $this->getFieldsUnderTest(true);
+
+        ob_start();
+        var_dump($fields);
+        $dump = ob_get_clean();
+
+        $this->assertLessThan(8192, strlen($dump));
+    }
+
+    /**
+     * @param bool $failOnMissingFields
+     *
+     * @return \Netgen\EzPlatformSiteApi\Core\Site\Values\Fields
+     */
+    protected function getFieldsUnderTest($failOnMissingFields)
     {
         return new Fields(
             $this->getMockedContent(),
             $this->getDomainObjectMapper(),
-            true
+            $failOnMissingFields,
+            $this->getLoggerMock()
         );
+    }
+
+    /**
+     * @return \PHPUnit\Framework\MockObject\MockObject|\Psr\Log\LoggerInterface
+     */
+    protected function getLoggerMock()
+    {
+        if ($this->loggerMock !== null) {
+            return $this->loggerMock;
+        }
+
+        $this->loggerMock = $this->getMockBuilder(LoggerInterface::class)->getMock();
+
+        return $this->loggerMock;
     }
 
     /**
@@ -95,6 +336,7 @@ class FieldsTest extends TestCase
     {
         return new Content(
             [
+                'id' => 1,
                 'site' => $this->getSiteMock(),
                 'domainObjectMapper' => $this->getDomainObjectMapper(),
                 'repository' => $this->getRepositoryMock(),
@@ -169,7 +411,7 @@ class FieldsTest extends TestCase
      */
     protected function getFieldTypeServiceMock()
     {
-        if (null !== $this->fieldTypeServiceMock) {
+        if ($this->fieldTypeServiceMock !== null) {
             return $this->fieldTypeServiceMock;
         }
 
@@ -190,7 +432,7 @@ class FieldsTest extends TestCase
      */
     protected function getFieldTypeMock()
     {
-        if (null !== $this->fieldTypeMock) {
+        if ($this->fieldTypeMock !== null) {
             return $this->fieldTypeMock;
         }
 
@@ -227,6 +469,14 @@ class FieldsTest extends TestCase
                         new FieldDefinition([
                             'id' => 1,
                             'identifier' => 'first',
+                        ]),
+                        new FieldDefinition([
+                            'id' => 2,
+                            'identifier' => 'second',
+                        ]),
+                        new FieldDefinition([
+                            'id' => 3,
+                            'identifier' => 'third',
                         ]),
                     ],
                 ])
@@ -268,6 +518,20 @@ class FieldsTest extends TestCase
                 'languageCode' => 'eng-GB',
                 'fieldTypeIdentifier' => 'ezinteger',
             ]),
+            new RepoField([
+                'id' => 2,
+                'fieldDefIdentifier' => 'second',
+                'value' => new Value(2),
+                'languageCode' => 'eng-GB',
+                'fieldTypeIdentifier' => 'ezinteger',
+            ]),
+            new RepoField([
+                'id' => 3,
+                'fieldDefIdentifier' => 'third',
+                'value' => new Value(3),
+                'languageCode' => 'eng-GB',
+                'fieldTypeIdentifier' => 'ezinteger',
+            ]),
         ];
 
         return $this->internalFields;
@@ -280,6 +544,7 @@ class FieldsTest extends TestCase
         }
 
         $repoContentInfo = new RepoContentInfo([
+            'id' => 1,
             'ownerId' => 'ownerId',
             'contentTypeId' => 'contentTypeId',
             'mainLanguageCode' => 'eng-GB',

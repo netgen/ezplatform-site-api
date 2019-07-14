aaf3a13676dd41eb3be269874fccdda62b237148
Petar Španja <petar@spanja.info>
2019-07-14 20:58:42 +0200

    Implement hinted search result extraction methods

--- lib/Core/Traits/SearchResultExtractorTrait.php
+++ lib/Core/Traits/SearchResultExtractorTrait.php
@@ -6,6 +6,8 @@ namespace Netgen\EzPlatformSiteApi\Core\Traits;
 
 use eZ\Publish\API\Repository\Values\Content\Search\SearchHit;
 use eZ\Publish\API\Repository\Values\Content\Search\SearchResult;
+use Netgen\EzPlatformSiteApi\API\Values\Content;
+use Netgen\EzPlatformSiteApi\API\Values\Location;
 
 /**
  * SearchResultExtractorTrait provides a way to extract value objects
@@ -27,6 +29,46 @@ trait SearchResultExtractorTrait
         return array_map(
             static function (SearchHit $searchHit) {
                 return $searchHit->valueObject;
+            },
+            $searchResult->searchHits
+        );
+    }
+
+    /**
+     * Extracts Content items from SearchResult.
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Search\SearchResult $searchResult
+     *
+     * @return \Netgen\EzPlatformSiteApi\API\Values\Content[]
+     */
+    protected function extractContentItems(SearchResult $searchResult): array
+    {
+        return array_map(
+            static function (SearchHit $searchHit): Content {
+                /** @var \Netgen\EzPlatformSiteApi\API\Values\Content $content */
+                $content = $searchHit->valueObject;
+
+                return $content;
+            },
+            $searchResult->searchHits
+        );
+    }
+
+    /**
+     * Extracts Locations from SearchResult.
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Search\SearchResult $searchResult
+     *
+     * @return \Netgen\EzPlatformSiteApi\API\Values\Location[]
+     */
+    protected function extractLocations(SearchResult $searchResult): array
+    {
+        return array_map(
+            static function (SearchHit $searchHit): Location {
+                /** @var \Netgen\EzPlatformSiteApi\API\Values\Location $location */
+                $location = $searchHit->valueObject;
+
+                return $location;
             },
             $searchResult->searchHits
         );
--- tests/lib/Integration/Traits/SearchResultExtractorStub.php
+++ tests/lib/Integration/Traits/SearchResultExtractorStub.php
@@ -16,8 +16,28 @@ class SearchResultExtractorStub
      *
      * @return \eZ\Publish\API\Repository\Values\ValueObject[]
      */
-    public function extract(SearchResult $searchResult): array
+    public function doExtractValueObjects(SearchResult $searchResult): array
     {
         return $this->extractValueObjects($searchResult);
+    }
+
+    /**
+     * @param \eZ\Publish\API\Repository\Values\Content\Search\SearchResult $searchResult
+     *
+     * @return \eZ\Publish\API\Repository\Values\ValueObject[]
+     */
+    public function doExtractContentItems(SearchResult $searchResult): array
+    {
+        return $this->extractContentItems($searchResult);
+    }
+
+    /**
+     * @param \eZ\Publish\API\Repository\Values\Content\Search\SearchResult $searchResult
+     *
+     * @return \eZ\Publish\API\Repository\Values\ValueObject[]
+     */
+    public function doExtractLocations(SearchResult $searchResult): array
+    {
+        return $this->extractLocations($searchResult);
     }
 }

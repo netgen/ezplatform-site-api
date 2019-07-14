8dccb7f05c10c6b16e18fc19f28983d6e515bf76
Petar Španja <petar@spanja.info>
2019-07-14 20:58:42 +0200

    Implement hinted search result extraction methods

--- tests/lib/Integration/Traits/SearchResultExtractorTraitTest.php
+++ tests/lib/Integration/Traits/SearchResultExtractorTraitTest.php
@@ -23,7 +23,7 @@ class SearchResultExtractorTraitTest extends BaseTest
         $this->stub = new SearchResultExtractorStub();
     }
 
-    public function testItExtractsValuesFromLocations(): void
+    public function testItExtractsValuesFromLocationSearchResult(): void
     {
         $locationIds = [5, 56];
         $findService = $this->getSite()->getFindService();
@@ -35,7 +35,7 @@ class SearchResultExtractorTraitTest extends BaseTest
 
         $searchResult = $findService->findLocations($query);
 
-        $locationValueObjects = $this->stub->extract($searchResult);
+        $locationValueObjects = $this->stub->doExtractValueObjects($searchResult);
 
         foreach ($locationValueObjects as $value) {
             $this->assertInstanceOf(Location::class, $value);
@@ -54,13 +54,13 @@ class SearchResultExtractorTraitTest extends BaseTest
 
         $searchResult = $findService->findLocations($query);
 
-        $locationValueObjects = $this->stub->extract($searchResult);
+        $locationValueObjects = $this->stub->doExtractValueObjects($searchResult);
 
         $this->assertIsArray($locationValueObjects);
         $this->assertEmpty($locationValueObjects);
     }
 
-    public function testItExtractsValuesFromContents(): void
+    public function testItExtractsValuesFromContentSearchResult(): void
     {
         $contentIds = [4, 54];
         $findService = $this->getSite()->getFindService();
@@ -72,7 +72,7 @@ class SearchResultExtractorTraitTest extends BaseTest
 
         $searchResult = $findService->findContent($query);
 
-        $contentValueObjects = $this->stub->extract($searchResult);
+        $contentValueObjects = $this->stub->doExtractValueObjects($searchResult);
 
         foreach ($contentValueObjects as $value) {
             $this->assertInstanceOf(Content::class, $value);
@@ -91,9 +91,87 @@ class SearchResultExtractorTraitTest extends BaseTest
 
         $searchResult = $findService->findContent($query);
 
-        $contentValueObjects = $this->stub->extract($searchResult);
+        $contentValueObjects = $this->stub->doExtractValueObjects($searchResult);
 
         $this->assertIsArray($contentValueObjects);
         $this->assertEmpty($contentValueObjects);
+    }
+
+    public function testItExtractsContentItemsFromContentSearchResult(): void
+    {
+        $contentIds = [4, 41];
+        $findService = $this->getSite()->getFindService();
+        $query = new Query(
+            [
+                'filter' => new Query\Criterion\ContentId($contentIds),
+            ]
+        );
+
+        $searchResult = $findService->findContent($query);
+
+        $contentValueObjects = $this->stub->doExtractContentItems($searchResult);
+
+        $this->assertCount(2, $contentValueObjects);
+
+        foreach ($contentValueObjects as $value) {
+            $this->assertInstanceOf(Content::class, $value);
+        }
+    }
+
+    public function testItExtractsContentItemsFromEmptyContentSearchResult(): void
+    {
+        $contentIds = [52];
+        $findService = $this->getSite()->getFindService();
+        $query = new Query(
+            [
+                'filter' => new Query\Criterion\ContentId($contentIds),
+            ]
+        );
+
+        $searchResult = $findService->findContent($query);
+
+        $contentValueObjects = $this->stub->doExtractContentItems($searchResult);
+
+        $this->assertIsArray($contentValueObjects);
+        $this->assertEmpty($contentValueObjects);
+    }
+
+    public function testItExtractsLocationsFromLocationSearchResult(): void
+    {
+        $locationIds = [5, 12];
+        $findService = $this->getSite()->getFindService();
+        $query = new LocationQuery(
+            [
+                'filter' => new Query\Criterion\LocationId($locationIds),
+            ]
+        );
+
+        $searchResult = $findService->findLocations($query);
+
+        $locationValueObjects = $this->stub->doExtractLocations($searchResult);
+
+        $this->assertCount(2, $locationValueObjects);
+
+        foreach ($locationValueObjects as $value) {
+            $this->assertInstanceOf(Location::class, $value);
+        }
+    }
+
+    public function testItExtractsLocationsFromEmptyLocationSearchResult(): void
+    {
+        $locationIds = [54];
+        $findService = $this->getSite()->getFindService();
+        $query = new LocationQuery(
+            [
+                'filter' => new Query\Criterion\LocationId($locationIds),
+            ]
+        );
+
+        $searchResult = $findService->findLocations($query);
+
+        $locationValueObjects = $this->stub->doExtractLocations($searchResult);
+
+        $this->assertIsArray($locationValueObjects);
+        $this->assertEmpty($locationValueObjects);
     }
 }

2889da3e07c0fd5adcdc7060762d970ba33a4324
Petar Španja <petar@spanja.info>
2019-07-27 14:48:55 +0200

    Remove useless allowed values validation

--- lib/Core/Site/QueryType/Location.php
+++ lib/Core/Site/QueryType/Location.php
@@ -27,41 +27,7 @@ abstract class Location extends Base
         ]);
 
         $resolver->setAllowedTypes('parent_location_id', ['int', 'string', 'array']);
-        $resolver->setAllowedValues(
-            'parent_location_id',
-            static function ($ids): bool {
-                if (!is_array($ids)) {
-                    return true;
-                }
-
-                foreach ($ids as $id) {
-                    if (!is_int($id) && !is_string($id)) {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-        );
-
         $resolver->setAllowedTypes('subtree', ['string', 'array']);
-        $resolver->setAllowedValues(
-            'subtree',
-            static function ($subtrees): bool {
-                if (!is_array($subtrees)) {
-                    return true;
-                }
-
-                foreach ($subtrees as $subtree) {
-                    if (!is_string($subtree)) {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-        );
-
         $resolver->setAllowedTypes('depth', ['int', 'array']);
         $resolver->setAllowedTypes('priority', ['int', 'array']);
 

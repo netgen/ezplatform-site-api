0660cef36e40e7bb41e14fffb3eb2b051f6d34e5
Mario Blažek <blazek.mario@gmail.com>
2019-08-13 10:17:42 +0200

    Add missing final and framework-extra-bundle as dependency

--- bundle/Converter/LocationParamConverter.php
+++ bundle/Converter/LocationParamConverter.php
@@ -6,7 +6,7 @@ namespace Netgen\Bundle\EzPlatformSiteApiBundle\Converter;
 
 use Netgen\EzPlatformSiteApi\API\Values\Location;
 
-class LocationParamConverter extends SiteParamConverter
+final class LocationParamConverter extends SiteParamConverter
 {
     protected function getSupportedClass(): string
     {

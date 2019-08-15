c8c63735dfdb6d1a6364766a4e967aa7e4a66024
Petar Španja <petar@spanja.info>
2019-08-15 17:02:25 +0200

    Implement local Matcher factory

---
+++ bundle/Matcher/MatcherFactory.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\EzPlatformSiteApiBundle\Matcher;
+
+use eZ\Bundle\EzPublishCoreBundle\Matcher\ServiceAwareMatcherFactory;
+use eZ\Publish\API\Repository\Repository;
+use eZ\Publish\Core\MVC\ConfigResolverInterface;
+use eZ\Publish\Core\MVC\Symfony\View\View;
+
+class MatcherFactory extends ServiceAwareMatcherFactory
+{
+    /**
+     * @var \eZ\Publish\Core\MVC\ConfigResolverInterface
+     */
+    private $configResolver;
+
+    /**
+     * @var string
+     */
+    private $parameterName;
+
+    /**
+     * @var string|null
+     */
+    private $namespace;
+
+    /**
+     * @var string|null
+     */
+    private $scope;
+
+    public function __construct(
+        Repository $repository,
+        string $relativeNamespace,
+        ConfigResolverInterface $configResolver,
+        string $parameterName,
+        ?string $namespace = null,
+        ?string $scope = null
+    ) {
+        $this->configResolver = $configResolver;
+        $this->parameterName = $parameterName;
+        $this->namespace = $namespace;
+        $this->scope = $scope;
+
+        parent::__construct($repository, $relativeNamespace);
+    }
+
+    public function match(View $view): ?array
+    {
+        $matchConfig = $this->configResolver->getParameter($this->parameterName, $this->namespace, $this->scope);
+        $this->setMatchConfig($matchConfig);
+
+        return parent::match($view);
+    }
+}

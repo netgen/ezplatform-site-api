29ce8a182cda9642ffbcd838c10332e6f862352d
Petar Španja <petar@spanja.info>
2019-10-04 15:10:37 +0200

    Adapt MatcherFactory not to depend on final class

--- bundle/Matcher/MatcherFactory.php
+++ bundle/Matcher/MatcherFactory.php
@@ -4,13 +4,24 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\EzPlatformSiteApiBundle\Matcher;
 
-use eZ\Bundle\EzPublishCoreBundle\Matcher\ServiceAwareMatcherFactory;
+use eZ\Bundle\EzPublishCoreBundle\Matcher\ViewMatcherRegistry;
 use eZ\Publish\API\Repository\Repository;
 use eZ\Publish\Core\MVC\ConfigResolverInterface;
+use eZ\Publish\Core\MVC\Symfony\Matcher\ClassNameMatcherFactory;
+use eZ\Publish\Core\MVC\Symfony\Matcher\MatcherInterface;
+use eZ\Publish\Core\MVC\Symfony\Matcher\ViewMatcherInterface;
 use eZ\Publish\Core\MVC\Symfony\View\View;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-class MatcherFactory extends ServiceAwareMatcherFactory
+class MatcherFactory extends ClassNameMatcherFactory
 {
+    use ContainerAwareTrait;
+
+    /**
+     * @var \eZ\Bundle\EzPublishCoreBundle\Matcher\ViewMatcherRegistry|null
+     */
+    private $viewMatcherRegistry;
+
     /**
      * @var \eZ\Publish\Core\MVC\ConfigResolverInterface
      */
@@ -32,6 +43,7 @@ class MatcherFactory extends ServiceAwareMatcherFactory
     private $scope;
 
     public function __construct(
+        ?ViewMatcherRegistry $viewMatcherRegistry,
         Repository $repository,
         string $relativeNamespace,
         ConfigResolverInterface $configResolver,
@@ -39,12 +51,36 @@ class MatcherFactory extends ServiceAwareMatcherFactory
         ?string $namespace = null,
         ?string $scope = null
     ) {
+        $this->viewMatcherRegistry = $viewMatcherRegistry;
         $this->configResolver = $configResolver;
         $this->parameterName = $parameterName;
         $this->namespace = $namespace;
         $this->scope = $scope;
 
         parent::__construct($repository, $relativeNamespace);
+    }
+
+    /**
+     * @param string $matcherIdentifier
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     *
+     * @return ViewMatcherInterface|MatcherInterface
+     */
+    protected function getMatcher($matcherIdentifier)
+    {
+        if ($this->viewMatcherRegistry !== null && strpos($matcherIdentifier, '@') === 0) {
+            return $this->viewMatcherRegistry->getMatcher(substr($matcherIdentifier, 1));
+        }
+
+        if ($this->container->has($matcherIdentifier)) {
+            /** @var ViewMatcherInterface|MatcherInterface $matcher */
+            $matcher = $this->container->get($matcherIdentifier);
+
+            return $matcher;
+        }
+
+        return parent::getMatcher($matcherIdentifier);
     }
 
     public function match(View $view): ?array

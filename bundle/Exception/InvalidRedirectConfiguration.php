ec1aaf0908c74f15c4c21c168015899c87833221
Petar Španja <petar@spanja.info>
2019-11-11 20:57:26 +0100

    Remove unused import

--- bundle/Exception/InvalidRedirectConfiguration.php
+++ bundle/Exception/InvalidRedirectConfiguration.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\EzPlatformSiteApiBundle\Exception;
 
-use Throwable;
-
 final class InvalidRedirectConfiguration extends \Exception
 {
     public function __construct(string $target)

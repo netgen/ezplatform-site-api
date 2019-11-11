768d232a95b7eccd54d22eb408cf6b9c2ebd221a
Ivan Herak <herak.ivan@gmail.com>
2019-11-11 11:47:19 +0100

    NGSTACK-347 throw exception in case of invalid configuration

---
+++ bundle/DependencyInjection/Compiler/InvalidConfigurationListenerPass.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\EzPlatformSiteApiBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+final class InvalidConfigurationListenerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        if (!$container->hasDefinition('netgen.ezplatform_site.event_listener.invalid_configuration')) {
+            return;
+        }
+
+        if ($container->getParameter('kernel.debug') === false) {
+            return;
+        }
+
+        $container->removeDefinition('netgen.ezplatform_site.event_listener.invalid_configuration');
+    }
+}
---
+++ bundle/EventListener/InvalidConfigurationListener.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\EzPlatformSiteApiBundle\EventListener;
+
+use eZ\Publish\Core\MVC\ConfigResolverInterface;
+use Netgen\Bundle\EzPlatformSiteApiBundle\Exception\InvalidRedirectConfiguration;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+final class InvalidConfigurationListener implements EventSubscriberInterface
+{
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var \Symfony\Component\Routing\Generator\UrlGeneratorInterface
+     */
+    protected $urlGenerator;
+
+    /**
+     * @var \eZ\Publish\Core\MVC\ConfigResolverInterface
+     */
+    protected $configResolver;
+
+    /**
+     * InvalidConfigurationListener constructor.
+     *
+     * @param \Symfony\Component\Routing\Generator\UrlGeneratorInterface $urlGenerator
+     * @param \eZ\Publish\Core\MVC\ConfigResolverInterface $configResolver
+     * @param \Psr\Log\LoggerInterface|null $logger
+     */
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        ConfigResolverInterface $configResolver,
+        LoggerInterface $logger = null
+    ) {
+        $this->urlGenerator = $urlGenerator;
+        $this->configResolver = $configResolver;
+        $this->logger = $logger ?: new NullLogger();
+    }
+
+
+    public static function getSubscribedEvents(): array
+    {
+        return [KernelEvents::EXCEPTION => 'onKernelException'];
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event): void
+    {
+        $exception = $event->getException();
+
+        if (!$exception instanceof InvalidRedirectConfiguration) {
+            return;
+        }
+
+        $this->logger->critical($exception->getMessage());
+
+        $rootLocationId = $this->configResolver->getParameter('content.tree_root.location_id');
+        $event->setResponse(
+            new RedirectResponse(
+                $this->urlGenerator->generate('ez_urlalias', ['locationId' => $rootLocationId]),
+                RedirectResponse::HTTP_FOUND
+            )
+        );
+    }
+}

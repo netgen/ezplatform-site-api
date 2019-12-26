68509022667febc8fa3de2fa1c1cb90ed7fa2b62
Petar Španja <petar@spanja.info>
2019-12-26 12:27:20 +0100

    NGSTACK-354: introduce ng_ez_view_content_embedded Twig function

---
+++ bundle/Templating/Twig/Extension/EzEmbeddedContentViewRuntime.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\EzPlatformSiteApiBundle\Templating\Twig\Extension;
+
+use eZ\Publish\Core\MVC\Symfony\View\Builder\ContentViewBuilder;
+use Netgen\Bundle\EzPlatformSiteApiBundle\View\ViewRenderer;
+
+/**
+ * Twig extension runtime for Site API embedded content view rendering.
+ */
+class EzEmbeddedContentViewRuntime
+{
+    /**
+     * @var \eZ\Publish\Core\MVC\Symfony\View\Builder\ContentViewBuilder
+     */
+    private $viewBuilder;
+
+    /**
+     * @var \Netgen\Bundle\EzPlatformSiteApiBundle\View\ViewRenderer
+     */
+    private $viewRenderer;
+
+    public function __construct(
+        ContentViewBuilder $viewBuilder,
+        ViewRenderer $viewRenderer
+    ) {
+        $this->viewBuilder = $viewBuilder;
+        $this->viewRenderer = $viewRenderer;
+    }
+
+    /**
+     * Renders the HTML for a given $content.
+     *
+     * @param string $viewType
+     * @param array $parameters
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     *
+     * @return string
+     */
+    public function renderEmbeddedContentView(string $viewType, array $parameters = []): string
+    {
+        $baseParameters = [
+            'viewType' => $viewType,
+            'layout' => false,
+            '_controller' => 'ez_content:embedAction',
+        ];
+
+        $view = $this->viewBuilder->buildView($baseParameters + $parameters);
+
+        return $this->viewRenderer->render($view, $parameters, false);
+    }
+}

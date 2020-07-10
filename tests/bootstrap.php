eedfbaf6ddf82474a25a08d277097ffc0e56224e
Edi Modrić <edi.modric@gmail.com>
2020-07-10 10:10:29 +0200

    Update tests setup for eZ Platform 3

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-$file = __DIR__ . '/../vendor/ezsystems/ezpublish-kernel/config.php';
+$file = __DIR__ . '/../vendor/ezsystems/ezplatform-kernel/config.php';
 if (!\file_exists($file)) {
     if (!\symlink($file . '-DEVELOPMENT', $file)) {
         throw new RuntimeException('Could not symlink config.php-DEVELOPMENT to config.php');

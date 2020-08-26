310a4a557af991b7ccd86701d05ee3b1854ae07d
Petar Španja <petar@spanja.info>
2020-08-26 10:14:19 +0200

    Improve naming

--- bundle/Events.php
+++ bundle/Events.php
@@ -7,10 +7,10 @@ namespace Netgen\Bundle\EzPlatformSiteApiBundle;
 final class Events
 {
     /**
-     * Dispatched when the view is rendered through "ng_view_content" Twig function,
-     * without using a sub-request.
+     * Dispatched when the view is rendered through ViewRenderer, without using a sub-request.
      *
-     * @see \Netgen\Bundle\EzPlatformSiteApiBundle\EventListener\ViewTaggerListener
+     * @see \Netgen\Bundle\EzPlatformSiteApiBundle\EventListener\RenderViewSubscriber
+     * @see \Netgen\Bundle\EzPlatformSiteApiBundle\View\ViewRenderer
      */
-    public const NG_VIEW_CONTENT_RENDER_VIEW = 'netgen_site_api.ng_view_content_render_view';
+    public const RENDER_VIEW = 'netgen_site_api.render_view';
 }

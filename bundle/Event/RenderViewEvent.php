3591a2b184c581150738d1e8fc2d78b20f9fa650
Petar Španja <petar@spanja.info>
2020-08-26 09:24:13 +0200

    Extend deprecated event object

--- bundle/Event/RenderViewEvent.php
+++ bundle/Event/RenderViewEvent.php
@@ -4,25 +4,8 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\EzPlatformSiteApiBundle\Event;
 
-use eZ\Publish\Core\MVC\Symfony\View\View;
-use Symfony\Component\EventDispatcher\Event;
+use Netgen\EzPlatformSiteApi\Event\RenderContentEvent;
 
-final class RenderViewEvent extends Event
+final class RenderViewEvent extends RenderContentEvent
 {
-    /**
-     * View object that was rendered.
-     *
-     * @var \eZ\Publish\Core\MVC\Symfony\View\View
-     */
-    private $view;
-
-    public function __construct(View $view)
-    {
-        $this->view = $view;
-    }
-
-    public function getView(): View
-    {
-        return $this->view;
-    }
 }

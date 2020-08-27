8bec6c2c77266db036d2da2e32fe78cf7bdbf702
Petar Španja <petar@spanja.info>
2020-08-27 10:04:51 +0200

    Extend new event from deprecated event

--- bundle/Event/RenderViewEvent.php
+++ bundle/Event/RenderViewEvent.php
@@ -4,8 +4,25 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\EzPlatformSiteApiBundle\Event;
 
-use Netgen\EzPlatformSiteApi\Event\RenderContentEvent;
+use eZ\Publish\Core\MVC\Symfony\View\View;
+use Symfony\Component\EventDispatcher\Event;
 
-final class RenderViewEvent extends RenderContentEvent
+class RenderViewEvent extends Event
 {
+    /**
+     * View object that was rendered.
+     *
+     * @var \eZ\Publish\Core\MVC\Symfony\View\View
+     */
+    private $view;
+
+    public function __construct(View $view)
+    {
+        $this->view = $view;
+    }
+
+    public function getView(): View
+    {
+        return $this->view;
+    }
 }
--- lib/Event/RenderContentEvent.php
+++ lib/Event/RenderContentEvent.php
@@ -4,30 +4,13 @@ declare(strict_types=1);
 
 namespace Netgen\EzPlatformSiteApi\Event;
 
-use eZ\Publish\Core\MVC\Symfony\View\View;
-use Symfony\Component\EventDispatcher\Event;
+use Netgen\Bundle\EzPlatformSiteApiBundle\Event\RenderViewEvent;
 
 /**
  * @deprecated since 3.5, to be removed in 4.0. Use RenderViewEvent instead.
  *
  * @see \Netgen\Bundle\EzPlatformSiteApiBundle\Event\RenderViewEvent
  */
-class RenderContentEvent extends Event
+class RenderContentEvent extends RenderViewEvent
 {
-    /**
-     * View object that was rendered.
-     *
-     * @var \eZ\Publish\Core\MVC\Symfony\View\View
-     */
-    private $view;
-
-    public function __construct(View $view)
-    {
-        $this->view = $view;
-    }
-
-    public function getView(): View
-    {
-        return $this->view;
-    }
 }

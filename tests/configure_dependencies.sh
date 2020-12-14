e1ea832370335d5827b196a063eacf5cb0699bea
Petar Španja <petar@spanja.info>
2020-12-14 07:59:33 +0100

    Migrate Travis to GitHub Actions

--- tests/configure_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ "$KERNEL_VERSION" != "" ] ; then
-    echo "> Update ezsystems/ezpublish-kernel requirement to ${KERNEL_VERSION}"
-    composer require --no-update ezsystems/ezpublish-kernel="${KERNEL_VERSION}"
-fi
